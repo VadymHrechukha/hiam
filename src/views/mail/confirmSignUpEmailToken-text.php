b1a2cf463c8fddfc9cded9a73e95549e61308537
strorch <smy980807@ukr.net>
2020-04-06 17:01:27 +0300

    minor template fix

--- src/views/mail/confirmSignUpEmailToken-text.php
+++ src/views/mail/confirmSignUpEmailToken-text.php
@@ -4,7 +4,7 @@
 /** @var yii\web\IdentityInterface $user */
 /** @var string $confirmLink */
 ?>
-<?= Yii::t('hiam', 'Hello {name},', ['name' => $user->name]) ?>
+<?= Yii::t('hiam', 'Hello {name},', ['name' => $user->username]) ?>
 
 <?= Yii::t('hiam', 'Follow the link below to confirm your email:') ?>
 
