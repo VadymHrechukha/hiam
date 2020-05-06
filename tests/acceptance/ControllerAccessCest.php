26a59d66205bc9f996d0b2c1908576a3e3668119
strorch <smy980807@ukr.net>
2020-05-06 13:05:03 +0300

    tests: created ControllerAccessCest

--- tests/acceptance/ControllerAccessCest.php
+++ tests/acceptance/ControllerAccessCest.php
@@ -11,80 +11,85 @@ final class ControllerAccessCest extends BasicHiamActions
      * @param AcceptanceTester $I
      * @throws \Exception
      */
-//    public function checkGuestActionsWhileGuest(AcceptanceTester $I): void
-//    {
-//        $I->wantTo('check authenticated actions while guest');
-//        $actions = [
-//            'signup', 'login', 'remote-proceed',
-//            'confirm-password', 'restore-password', 'reset-password',
-//            'terms', 'privacy-policy',
-//        ];
-//        foreach ($actions as $action) {
-//            $I->amOnPage('/site/' . $action);
-//            $I->wait(3);
-//        }
-//    }
-//
-//    /**
-//     * @before login
-//     * @param AcceptanceTester $I
-//     * @throws \Exception
-//     */
-//    public function checkGuestActionsWhileAuthenticated(AcceptanceTester $I): void
-//    {
-//        $I->wantTo('check authenticated actions while guest');
-//        $actions = [
-//            'signup', 'login', 'remote-proceed',
-//            'confirm-password', 'restore-password', 'reset-password',
-//            'terms', 'privacy-policy',
-//        ];
-//        foreach ($actions as $action) {
-//            $I->amOnPage('/site/' . $action);
-//            $I->wait(3);
-//        }
-//    }
+    public function checkGuestActionsWhileGuest(AcceptanceTester $I): void
+    {
+        $I->wantTo('check guest actions while guest');
+        $actions = [
+            'signup'            => 'Sign up',
+            'login'             => 'Sign in',
+            'restore-password'  => 'Forgot password',
+            'reset-password'    => 'Failed reset password',
+            'terms'             => 'Terms of Service Agreement',
+            'privacy-policy'    => 'PRIVACY POLICY',
+        ];
+        foreach ($actions as $action => $text) {
+            $I->amOnPage('/site/' . $action);
+            $I->waitForText($text);
+        }
+    }
 
+    /**
+     * @before login
+     * @param AcceptanceTester $I
+     * @throws \Exception
+     */
+    public function checkGuestActionsWhileAuthenticated(AcceptanceTester $I): void
+    {
+        $I->wantTo('check guest actions while authenticated');
+        $actions = ['signup', 'login', 'remote-proceed', 'confirm-password', 'restore-password', 'reset-password'];
 
-
-
-
-
+        $userInfo = $this->getUserInfo();
+        foreach ($actions as $action) {
+            $I->amOnPage('/site/' . $action);
+            $I->waitForText($userInfo['username']);
+        }
+    }
 
     /**
      * @param AcceptanceTester $I
      * @throws \Exception
      */
-//    public function chechAuthenticatedActionsWhileGuest(AcceptanceTester $I): void
-//    {
-//        $I->wantTo('check authenticated actions while guest');
-//        $actions = [
-//            'lockscreen',
-//            'resend-verification-email', 'back',
-//            'change-password', 'change-email',
-//        ];
-//        foreach ($actions as $action) {
-//            $I->amOnPage('/site/' . $action);
-//            $I->see('Sign in');
-//        }
-//    }
-//
-//    /**
-//     * @param AcceptanceTester $I
-//     * @throws \Exception
-//     */
-//    public function chechAuthenticatedActionsWhileAuthenticated(AcceptanceTester $I): void
-//    {
-//        $I->wantTo('check authenticated actions while guest');
-//        $actions = [
-//            'lockscreen',
-//            'resend-verification-email', 'back',
-//            'change-password', 'change-email',
-//        ];
-//        foreach ($actions as $action) {
-//            $I->amOnPage('/site/' . $action);
-//            $I->see('Sign in');
-//        }
-//    }
+    public function chechAuthenticatedActionsWhileGuest(AcceptanceTester $I): void
+    {
+        $I->wantTo('check authenticated actions while guest');
+        $actions = ['lockscreen', 'resend-verification-email', 'back', 'change-password', 'change-email'];
+        foreach ($actions as $action) {
+            $I->amOnPage('/site/' . $action);
+            $I->waitForText('Sign in');
+        }
+    }
+
+    /**
+     * @before login
+     * @param AcceptanceTester $I
+     * @throws \Exception
+     */
+    public function chechAuthenticatedActionsWhileAuthenticated(AcceptanceTester $I): void
+    {
+        $I->wantTo('check authenticated actions while authenticated');
+        $usersInfo = $this->getUserInfo();
+        $actions = [
+            'lockscreen'                => $usersInfo['username'],
+            'resend-verification-email' => 'Please confirm your email address!',
+            'back'                      => $usersInfo['username'],
+            'change-password'           => 'Enter your new password',
+            'change-email'              => 'Enter your new email',
+        ];
+
+        foreach ($actions as $action => $text) {
+            $I->amOnPage('/site/' . $action);
+            $I->waitForText($text);
+        }
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @throws \Exception
+     */
+    protected function login(AcceptanceTester $I)
+    {
+        $this->doLogin($I, $this->getUserInfo());
+    }
 
     /**
      * @inheritDoc
