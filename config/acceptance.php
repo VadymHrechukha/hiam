9e50c31f173ed3ca46c5970c762035390cbb4837
strorch <smy980807@ukr.net>
2020-07-01 15:14:35 +0300

    tests: using php config from core

--- config/acceptance.php
+++ config/acceptance.php
@@ -10,7 +10,7 @@
 
 return [
     'PARAMS_LOCATION' => dirname(__DIR__, 4) . '/vendor/yiisoft/composer-config-plugin-output/acceptance.php',
-    'YII2_CONFIG_LOCATION' => dirname(__DIR__, 4) . '/tests/acceptance/config/suite.php',
+    'YII2_CONFIG_LOCATION' => dirname(__DIR__) . '/tests/acceptance/config/suite.php',
 
     'COMMON_TESTS_LOCATION' => dirname(__DIR__, 4) . '/tests',
     'COMMON_ACCEPTANCE_SUITE' => dirname(__DIR__, 4) . '/tests/acceptance.suite.yml',
